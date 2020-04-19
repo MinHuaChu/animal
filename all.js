a8b58de1cc71d178fd0bc010c797ecd1e0b10ae5
MinHuaChu <sumi0309546@gmail.com>
2020-04-19 20:27:12 +0800

    dddd

--- all.js
+++ all.js
@@ -1,3 +1,36 @@
 $(document).ready(function () {
-    $(".jk").hide();
+    $('.top a').click(function(event) {
+        event.preventDefault();
+        $('html,body').animate({
+          scrollTop: 0
+        }, 1000);
+       });
+
+       $('.bottom1').click(function (event) { 
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop:900
+          }, 1000);
+    });
+
+    $('.bottom2').click(function (event) { 
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop:2300
+          }, 1500);
+    });
+
+    $('.bottom3').click(function (event) { 
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop:3100
+          }, 1700);
+    });
+
+    $('.bottom4').click(function (event) { 
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop:3800
+          }, 2000);
+    });
 });
